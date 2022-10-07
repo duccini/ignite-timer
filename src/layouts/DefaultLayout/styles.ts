b1efff9f9e8d1e69d5f0c8e6ddc6d41090317375
Guilherme Duccini <guilherme@guilhermeduccini.com.br>
2022-10-07 11:14:21 -0300

    history page

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 6rem);
+  min-height: calc(100vh - 6rem);
 
   margin: 3rem auto;
   padding: 2.5rem;
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,102 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  //padding: 3rem 3rem 1.5rem;
+  padding: 2rem 3rem 0;
+
+  //border: 1px solid green;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background: ${(props) => props.theme['gray-900']};
+      padding: 1rem;
+
+      text-align: left;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        border-top-left-radius: 8px;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+        border-top-right-radius: 8px;
+      }
+    }
+
+    td {
+      background: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+
+      padding: 1rem;
+
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        padding-left: 1.5rem;
+        width: 50%;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+// const STATUS_COLORS = {
+//   yellow: '#FFC125',
+//   green: '#006400',
+//   red: '#8B0000',
+// }
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+    /* background: ${(props) => STATUS_COLORS[props.statusColor]}; */
+  }
+`
